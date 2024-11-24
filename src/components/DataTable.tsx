f83e2b3888b3c971af4a2c2c4cc8b95f67e14fa9
darkdecaydence <alkii900@msn.com>
2024-11-24 20:50:42 +0100

    [bugfix] fix 'duration' not formatted as time string.

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -14,7 +14,7 @@ import {extractVideoIdFromUrl, parseVodDataJson} from "../utils/vod-data-parser.
 import {useNavigate} from "react-router-dom";
 import jsonDataList from "../assets/clip-data.json"
 import {Temporal} from "@js-temporal/polyfill";
-import {parseTimeStringToDuration} from "../utils/time-conversion.ts";
+import {parseDurationToTimeString, parseTimeStringToDuration} from "../utils/time-conversion.ts";
 
 function DataTable() {
     const {settingsData} = useContext(SettingsContext);
@@ -153,13 +153,13 @@ function retrieveQuanta() {
             startSeconds: startTemporal.total("seconds"),
             end: jsonQuantum.end,
             endSeconds: endTemporal.total("seconds"),
-            duration: endTemporal.subtract(startTemporal).toString(),
+            duration: parseDurationToTimeString(endTemporal.subtract(startTemporal)),
             tags: jsonQuantum.tags,
             ...(await parseVodDataJson(`stream-data/${videoId}.info.json`) ?? {})
         }
     })).then(researchQuanta => researchQuanta.map(researchQuantum => {
         return {
-            vodTitle: researchQuantum.title ?? "N/A",
+            vodTitle: researchQuantum.title ?? extractVideoIdFromUrl(researchQuantum.videoUrl),
             vodFriendlyDate: researchQuantum.releaseTimestamp 
                 ? Temporal.Instant
                     .fromEpochSeconds(researchQuantum.releaseTimestamp)
